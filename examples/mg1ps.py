9977e71fb7361847d91168c85635cadef8777b8f
MilanVeul <milan@veul.net>
2026-04-21 14:49:23 +0200

    Cleaned up repo, made sure the examples run

--- examples/mg1ps.py
+++ examples/mg1ps.py
@@ -20,7 +20,7 @@ import random
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 
 from des_library import Simulation, Event, TimeWeightedStatistic, SampleStatistic
 
