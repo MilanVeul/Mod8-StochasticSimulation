9343cf4fd0788d1ec98aef3b9c925a878c0859a9
MilanVeul <milan@veul.net>
2026-04-22 12:18:17 +0200

    Working model without statisticss

--- assignment1/charging_station.py
+++ assignment1/charging_station.py
@@ -1,10 +1,10 @@
+from __future__ import annotations
 import random
 import os
 import sys
 import math
 import bisect
-from typing import List
-from __future__ import annotations
+from typing import List, Optional
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 
@@ -14,67 +14,85 @@ from des_library import Simulation, Event, TimeWeightedStatistic, SampleStatisti
 class Vehicle:
     def __init__(
             self, 
+            vehicle_number: int,
             arrival_time: int, 
-            battery_percentage: float, 
-            reneging_event: RenegingEvent
+            battery_percentage: float
         ):
+        self.number = vehicle_number
         self.arrival_time = arrival_time
         self.battery_percentage = battery_percentage
-        self.reneging_event: RenegingEvent = reneging_event
         self.depature_event: DepartureEvent = None
+        self.reneging_event: RenegingEvent = None
         self.charging_start: int = -1
 
 
 class ChargingStationModel:
     def __init__(self, num_stations: int = 4, termination_number: int = 800, seed: int = 70):
-        self.seed = seed
+        random.seed(seed)
+
         self.termination_number = termination_number
         self.sim = Simulation()
         self.num_stations = num_stations
 
-        self.stations: List[Vehicle] = [None] * num_stations
-        self.num_vehicles_charging = 0
+        self.stations: List[Vehicle] = []
         self.queue: List[Vehicle] = []
         
-        self.completions: int = 0 
+        self.completions: int = 0
 
-        self.init_arrivals(3000)
+    def run(self):
+        schedule_arrival(self.sim, 0, self)
+        self.sim.run()
     
-    def init_arrivals(self, num_arrivals):
-        prev_time = 0
-        for n in range(num_arrivals):
-            time = prev_time + 15 * (1 + math.sin(n*math.pi / 12))**2 + 2
-            arrival_event = ArrivalEvent(time, self, n)
-            self.sim.schedule(arrival_event)
-            prev_time = time
-    
+    def queue_vehicle(self, vehicle):
+        self.queue.append(vehicle)
+
     def leave_queue(self, vehicle):
         self.queue.remove(vehicle)
 
     def charge_vehicle(self, vehicle):
-        if self.num_vehicles_charging >= self.num_stations: return
-        self.queue.append(vehicle)
-        self.num_vehicles_charging += 1
+        if len(self.stations) >= self.num_stations: 
+            raise Exception("Station is full!")
+        self.stations.append(vehicle)
+        print(f"Vehicle {vehicle.number} charges. Status: {len(self.stations)}/{self.num_stations}")
 
-    def end_service(self, leaving_vehicle: Vehicle) -> Vehicle:
-        self.stations.remove(leaving_vehicle)
+    def leave_charging(self, leaving_vehicle: Vehicle) -> Optional[Vehicle]:
+        """Removes vehicle from charging stations and removes and returns the first vehicle in queue."""
         
+        self.stations.remove(leaving_vehicle)
+
+        first_vehicle = None
         if len(self.queue) > 0:
-            first_vehicle = self.queue.pop(0)
-            self.stations.append(first_vehicle)
-        else:
-            self.num_vehicles_charging -= 1
+            first_vehicle = self.queue[0]
 
         self.completions += 1
         if self.completions >= self.termination_number:
             # TODO: Report statistics
+            print("Simulation ended")
             self.sim.stop()
+            return None
         return first_vehicle
+    
+    @property
+    def has_capacity(self) -> bool:
+        return len(self.stations) < self.num_stations
+    
 
 ######################################
 
+def schedule_arrival(sim: Simulation, vehicle_number: int, model: ChargingStationModel):
+    time = sim.current_time + 15 * (1 + math.sin(vehicle_number*math.pi / 12))**2 + 2
+    arrival_event = ArrivalEvent(time, model, vehicle_number+1)
+    sim.schedule(arrival_event)
+
 def get_charging_duration(vehicle: Vehicle):
     return 60 * (1-vehicle.battery_percentage)
+
+def schedule_departure_event(sim: Simulation, vehicle: Vehicle, model: ChargingStationModel):
+    vehicle.charging_start = sim.current_time
+    dep_time = sim.current_time + get_charging_duration(vehicle)
+    dep_event = DepartureEvent(dep_time, vehicle, model)
+    vehicle.depature_event = dep_event
+    sim.schedule(dep_event)
 
 class ArrivalEvent(Event):
     def __init__(self, time: float, model: ChargingStationModel, vehicle_number: int):
@@ -84,17 +102,23 @@ class ArrivalEvent(Event):
     
     def execute(self, sim: Simulation):
         if self.cancelled: return
+        print(f"Vehicle {self.vehicle_number} arrived.")
         battery_percentage = 0.5 * abs(math.sin(self.vehicle_number * math.pi / 7) + 1)
 
-        if len(self.model.queue) == 0:
-            new_vehicle = Vehicle(sim.current_time, battery_percentage, None)
+        if len(self.model.queue) == 0 and self.model.has_capacity:
+            new_vehicle = Vehicle(self.vehicle_number, sim.current_time, battery_percentage)
             self.model.charge_vehicle(new_vehicle)
             schedule_departure_event(sim, new_vehicle, self.model)
         else:
             patience = 20 * (1 + abs(math.cos(self.vehicle_number * math.e)))
-            reneging_event = RenegingEvent(sim.current_time + patience, self.model)
-            new_vehicle = Vehicle(sim.current_time, battery_percentage, reneging_event)
+            new_vehicle = Vehicle(self.vehicle_number, sim.current_time, battery_percentage)
+            reneging_event = RenegingEvent(sim.current_time + patience, new_vehicle, self.model)
+            new_vehicle.reneging_event = reneging_event
             self.model.queue_vehicle(new_vehicle)
+            sim.schedule(reneging_event)
+
+        # Schedule new arrival
+        schedule_arrival(sim, self.vehicle_number, self.model)
 
 class RenegingEvent(Event):
     def __init__(self, time: float, vehicle: Vehicle, model: ChargingStationModel):
@@ -104,15 +128,8 @@ class RenegingEvent(Event):
     
     def execute(self, sim: Simulation):
         if self.cancelled: return
+        print(f"A Vehicle renegated.")
         self.model.leave_queue(self.vehicle)
-
-def schedule_departure_event(sim: Simulation, vehicle: Vehicle, model: ChargingStationModel):
-    vehicle.set_charging_start(sim.current_time)
-    dep_time = sim.current_time + get_charging_duration(vehicle)
-    dep_event = DepartureEvent(dep_time, vehicle, model)
-    vehicle.depature_event = dep_event
-    sim.schedule(dep_event)
-
 
 class DepartureEvent(Event):
     def __init__(self, time: float, vehicle: Vehicle, model: ChargingStationModel):
@@ -122,10 +139,14 @@ class DepartureEvent(Event):
     
     def execute(self, sim: Simulation):
         if self.cancelled: return
+        print(f"Vehicle {self.vehicle.number} left.")
         # Replace vehicle in charging station
-        new_vehicle = self.model.end_service(self.vehicle)
+        new_vehicle = self.model.leave_charging(self.vehicle)
 
-        # Schedule new departure event
+        if not new_vehicle: return
+        new_vehicle.reneging_event.cancel()
+        self.model.leave_queue(new_vehicle)
+        self.model.charge_vehicle(new_vehicle)
         schedule_departure_event(sim, new_vehicle, self.model)
 
         # Handle early departure
@@ -135,6 +156,7 @@ class DepartureEvent(Event):
                 departure_time = veh.charging_start + 60 * (1-veh.battery_percentage)
                 if departure_time - sim.current_time <= 15: continue
                 if random.random(0,1) > 0.2: continue
+                print(f"Vehicle is departing early.")
                 # Cancel original departure event
                 veh.depature_event.cancel()
                 # Create early departure event
@@ -142,4 +164,9 @@ class DepartureEvent(Event):
                 early_dep_event = DepartureEvent(early_dep_time, veh, self.model)
                 veh.depature_event = early_dep_event
                 sim.schedule(early_dep_event)
+
+
+if __name__ == "__main__":
+    model = ChargingStationModel(4, 800)
+    model.run()
 
